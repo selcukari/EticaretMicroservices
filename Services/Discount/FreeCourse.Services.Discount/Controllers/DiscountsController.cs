22a59d66916b0f5b1adae13b5804c9cea68d5f9e
selcuk.ari <selcukari51@gmail.com>
2025-02-16 20:12:27 +0300

    discount create

---
+++ Services/Discount/FreeCourse.Services.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,12 @@
+﻿using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : CustomBaseController
+    {
+      
+    }
+}
