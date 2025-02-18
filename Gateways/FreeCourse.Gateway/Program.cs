afef29754571f662a643d4449772cef79a38930c
selcuk.ari <selcukari51@gmail.com>
2025-02-18 21:17:54 +0300

    gateway do

--- Gateways/FreeCourse.Gateway/Program.cs
+++ Gateways/FreeCourse.Gateway/Program.cs
@@ -5,12 +5,22 @@ using Ocelot.Values;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
-    .AddJsonFile($"configuration.{builder.Environment.EnvironmentName.ToLower()}.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"configuration.{builder.Environment.EnvironmentName.ToLower()}.json")
     .AddEnvironmentVariables();
+
+var deneme = builder.Environment.EnvironmentName.ToLower();
+
+builder.Services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_gateway";
+    options.RequireHttpsMetadata = false;
+});
+
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
 await app.UseOcelot();
 
-builder.Services.AddOcelot();
-app.Run();
+await app.RunAsync();
