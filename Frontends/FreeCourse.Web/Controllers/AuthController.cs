684b7e38ab8a2a9561c2a2e58f98dfea63c9325b
selcuk.ari <selcukari51@gmail.com>
2025-02-19 13:52:24 +0300

    sinup htmlcss do

---
+++ Frontends/FreeCourse.Web/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        //private readonly IIdentityService _identityService;
+
+        //public AuthController(IIdentityService identityService)
+        //{
+        //    _identityService = identityService;
+        //}
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
