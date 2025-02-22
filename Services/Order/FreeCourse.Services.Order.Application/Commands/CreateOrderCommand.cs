77c88918badbcdc0ba159030cd5f07499253bbfa
selcuk.ari <selcukari51@gmail.com>
2025-02-22 15:54:12 +0300

    order add service

--- Services/Order/FreeCourse.Services.Order.Application/Commands/CreateOrderCommand.cs
+++ Services/Order/FreeCourse.Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -11,7 +11,10 @@ namespace FreeCourse.Services.Order.Application.Commands
 {
     public class CreateOrderCommand : IRequest<Response<CreatedOrderDto>>
     {
+        // user'ın id'si
         public string BuyerId { get; set; }
+        // odeme numarası
+        // public int PaymentNo { get; set; }
 
         public List<OrderItemDto> OrderItems { get; set; }
 
